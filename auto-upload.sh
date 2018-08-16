2f1392ad3751228960bad7d23e591262288ff8b2
SuperShinningFinger <791174722@qq.com>
2018-08-16 11:22:08 +0800

    auto-update

--- auto-upload.sh
+++ auto-upload.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-UPDATE_PATH=~/shell
+UPDATE_PATH=~/shell/
 
 ping -c 1 114.114.114.114 > /dev/null 2>&1
 
@@ -9,15 +9,15 @@ if [ $? -eq 0 ];then
     cd UPDATE_PATH
     echo 'cd success'
     if [ ! -f update.log  ];then
-        touch update.log
+        `touch  ${UPDATE_PATH}update.log`
     fi
-    echo "commit date: `date`" >> update.log
-    echo -e "last commit info:" >> update.log
-    echo `git log -1` >> update.log
+    echo "auto commit date: `date`" >> ${UPDATE_PATH}update.log
+    echo -e "last commit info:" >> ${UPDATE_PATH}update.log
+    echo `git log -1` >>  ${UPDATE_PATH}update.log
     git add *
     git commit -m "auto-update"
     git push -f -u origin master
 else
     echo "网络链接异常，未能同步$UPDATE_PATH"
-    echo -e "\nInternet connection failed:  `date`\n" >> update.log
+    echo -e "\nInternet connection failed:  `date`\n" >>  ${UPDATE_PATH}update.log
 fi
